f62fd5d1ee1001cd4f2cac8a44c2d0a32f54fafa
RenanMarquetti <codeum.desenvolvimento@gmail.com>
2025-05-22 10:47:57 -0300

    add listener

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,14 @@ const myQueue = new Queue('foo');
 const connection = new IORedis({ maxRetriesPerRequest: null });
 const myWorker = new Worker('foo', async job => console.log(job.data), { connection });
 
+myWorker.on('completed', job => {
+  console.log(`${job.id} has completed!`);
+});
+
+myWorker.on('failed', (job, err) => {
+  console.log(`${job.id} has failed with ${err.message}`);
+});
+
 myQueue.add('job1', {name: "job1"});
 myQueue.add('job2', {name: "job2"});
 
